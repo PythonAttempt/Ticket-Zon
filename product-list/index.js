bdc225f2d723393edcc88699f7addd5c6a25db01
PythonAttempt <75542870+PythonAttempt@users.noreply.github.com>
2021-07-26 19:39:13 +0100

    reuploading index.js to fix

--- product-list/index.js
+++ product-list/index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const fastify = require('./server')({logger: true})
 
 const start = async() => {
@@ -9,18 +9,4 @@ const start = async() => {
         process.exit(1)
     }
 }
-
-=======
-const fastify = require('./server')({logger: true})
-
-const start = async() => {
-    try{
-        await fastify.listen(3000)
-    } catch (err){
-        fastify.log.error(err)
-        process.exit(1)
-    }
-}
-
->>>>>>> 564e81be5cfd3c7724e3ae040ebcb2d18286d35a
 start()
